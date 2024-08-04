e3a074a949b498df66113e0e5744992815645a1b
Santhosh <santhoshs19032003@gmail.com>
2024-08-04 10:23:53 +0530

    cors update

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-require("dotenv").config();
-const { ApolloServer } = require("apollo-server");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 
 const MONGODB = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 4000; // Default port if not specified
 
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require("./graphql/resolvers");
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true, // Enable introspection
+  cors: {
+    origin: '*', // Adjust according to your needs
+    methods: 'GET, POST',
+  },
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Mongodb Connection successful");
-    return server.listen({ port: process.env.PORT });
+    console.log('Mongodb Connection successful');
+    return server.listen({ port: PORT });
   })
-  .then((res) => {
-    console.log(`server running at ${res.url}`);
+  .then(({ url }) => {
+    console.log(`Server running at ${url}`);
+  })
+  .catch((error) => {
+    console.error('Error starting server:', error);
   });
